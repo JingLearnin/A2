1133f507217a5781cbf25a57a6b2f1cbb9c4d407
JingLearnin <160545616+JingLearnin@users.noreply.github.com>
2024-03-21 20:22:48 -0400

    Add test for default constructor and result in InformationTest.java

--- src/test/java/ca/mcmaster/se2aa4/island/team109/InformationTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team109/InformationTest.java
@@ -40,3 +40,37 @@ import static org.junit.jupiter.api.Assertions.*;
         assertEquals("unknown", info.getDistance(), "Default distance should be 'unknown'");
     }
 
+    @Test
+    public void testDefResults(){
+        Information info = new Information();
+        assertEquals("unknown", info.getResult());
+    }
+
+
+    @Test
+    public void testConstructorWithOneParameter() {
+        String result = "TestResult";
+        Information info = new Information(result);
+
+        assertEquals(result, info.getResult());
+        assertNull(info.getDistance(), "Distance should be null for one-parameter constructor");
+    }
+
+    @Test
+    public void testGetResult() {
+        Information info = new Information("TestResult");
+        assertEquals("TestResult", info.getResult());
+    }
+
+    @Test
+    public void testGetDistance() {
+        Information info = new Information("TestResult", "10");
+        assertEquals("10", info.getDistance());
+    }
+
+    @Test
+    public void testDefaultDistanceValue() {
+        Information info = new Information("TestResult");
+        assertEquals("unknown", info.getDistance(), "Default distance should be 'unknown'");
+    }
+
