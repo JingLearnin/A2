6d50dd2b00b8381639986178429b445dab0d1dd8
JingLearnin <160545616+JingLearnin@users.noreply.github.com>
2024-03-21 20:19:54 -0400

    Update InformationTest.java to include unit tests for Information class in 'information-test-update' branch

--- src/test/java/ca/mcmaster/se2aa4/island/team109/InformationTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team109/InformationTest.java
@@ -3,12 +3,40 @@ package ca.mcmaster.se2aa4.island.team109;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class InformationTest {
+@Test
+    public void testConstructorWithTwoParameters() {
+        String result = "TestResult";
+        String distance = "10";
+        Information info = new Information(result, distance);
+
+        assertEquals(result, info.getResult());
+        assertEquals(distance, info.getDistance());
+    }
 
     @Test
-    public void sampleTest() {
-    
-        assertFalse(2 == 1);
-    
+    public void testConstructorWithOneParameter() {
+        String result = "TestResult";
+        Information info = new Information(result);
+
+        assertEquals(result, info.getResult());
+        assertNull(info.getDistance(), "Distance should be null for one-parameter constructor");
     }
-}
+
+    @Test
+    public void testGetResult() {
+        Information info = new Information("TestResult");
+        assertEquals("TestResult", info.getResult());
+    }
+
+    @Test
+    public void testGetDistance() {
+        Information info = new Information("TestResult", "10");
+        assertEquals("10", info.getDistance());
+    }
+
+    @Test
+    public void testDefaultDistanceValue() {
+        Information info = new Information("TestResult");
+        assertEquals("unknown", info.getDistance(), "Default distance should be 'unknown'");
+    }
+
